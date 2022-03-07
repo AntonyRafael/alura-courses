c4b533fe2de81cae33cde23295bf2e4c0c5202d7
Antony Rafael <depaulacostaantony@gmail.com>
2022-03-07 08:44:19 -0300

    Box component

--- Vue/vue3-alura-tracker/src/components/Formulario.vue
+++ Vue/vue3-alura-tracker/src/components/Formulario.vue
@@ -27,6 +27,7 @@ import Temporizador from "./Temporizador.vue";
 export default defineComponent({
   name: "Formulario",
   components: { Temporizador },
+  emits: ['aoSalvarTarefa'],
   data() {
     return {
       descricao: "",
@@ -34,8 +35,10 @@ export default defineComponent({
   },
   methods: {
     finalizarTarefa(tempoDecorrido: number): void {
-      console.log("tempo", tempoDecorrido);
-      console.log("tarefa", this.descricao);
+      this.$emit('aoSalvarTarefa', {
+        duracaoEmSegundos: tempoDecorrido,
+        descricao: this.descricao
+      })
       this.descricao = ''
     },
   },
