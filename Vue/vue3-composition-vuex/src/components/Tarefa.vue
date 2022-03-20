9163d0a9e6d85e685301bb2b51bb2b9dc50d5c14
Antony Rafael <depaulacostaantony@gmail.com>
2022-03-20 12:07:59 -0300

    tarefa to composition API

--- Vue/vue3-composition-vuex/src/components/Tarefa.vue
+++ Vue/vue3-composition-vuex/src/components/Tarefa.vue
@@ -1,6 +1,6 @@
 <template>
-  <Box>
-    <div class="columns">
+  <Box >
+    <div class="columns clicavel" @click="tarefaClicada">
       <div class="column is-4">
         {{ tarefa.descricao || 'Tarefa sem descrição' }}
       </div>
@@ -15,13 +15,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType } from 'vue';
 import Cronometro from "./Cronometro.vue";
 import Box from "./Box.vue";
 import ITarefa from "../interfaces/ITarefa";
 
 export default defineComponent({
   name: 'Tarefa',
+  emits: ['aoTarefaClicada'],
   components: {
     Cronometro,
     Box
@@ -32,11 +33,18 @@ export default defineComponent({
       required: true
     }
   },
-  computed: {
-    tempoGasto () : string {
-      return new Date(this.tarefa.duracaoEmSegundos * 1000)
+  setup(props, { emit }) {
+    const tarefaClicada = () : void => {
+      emit('aoTarefaClicada', props.tarefa)
+    }
+    const tempoGasto = computed(() => {
+      return new Date(props.tarefa.duracaoEmSegundos * 1000)
         .toISOString()
         .substr(11, 8)
+    })
+    return {
+      tarefaClicada,
+      tempoGasto
     }
   }
 });
