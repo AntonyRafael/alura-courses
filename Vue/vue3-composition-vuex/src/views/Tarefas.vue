196bdb3c5d2544951689063dfa9b31dde8af77f0
Antony Rafael <depaulacostaantony@gmail.com>
2022-03-20 11:55:01 -0300

    filter tarefas

--- Vue/vue3-composition-vuex/src/views/Tarefas.vue
+++ Vue/vue3-composition-vuex/src/views/Tarefas.vue
@@ -5,6 +5,19 @@
       Você não está muito produtivo hoje
       <span class="has-text-weight-bold">:(</span>
     </Box>
+    <div class="field">
+      <p class="control has-icons-left">
+        <input
+          type="text"
+          placeholder="Digite para filtrar"
+          class="input"
+          v-model="filtro"
+        />
+        <span class="icon is-small is-left">
+          <i class="fas fa-search"></i>
+        </span>
+      </p>
+    </div>
     <Tarefa
       v-for="(tarefa, index) in tarefas"
       :tarefa="tarefa"
@@ -51,7 +64,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref, watchEffect } from "vue";
 import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
@@ -92,19 +105,34 @@ export default defineComponent({
         .then(() => this.fecharModal());
     },
   },
-  setup() {
-    const store = useStore();
-    store.dispatch(OBTER_TAREFAS);
-    store.dispatch(OBTER_PROJETOS);
-    return {
-      tarefas: computed(() => store.state.tarefa.tarefas),
-      store,
-    };
-  },
   computed: {
     semTarefas(): boolean {
       return this.tarefas.length == 0;
     },
   },
+  setup() {
+    const store = useStore();
+    store.dispatch(OBTER_TAREFAS);
+    store.dispatch(OBTER_PROJETOS);
+
+    const filtro = ref("");
+
+    // const tarefas = computed(() =>
+    //   store.state.tarefa.tarefas.filter(
+    //     (t) => !filtro.value || t.descricao.includes(filtro.value)
+    //   )
+    // );
+
+    watchEffect(() => {
+      store.dispatch(OBTER_TAREFAS, filtro.value)
+    })
+
+    return {
+      tarefas: computed(() => store.state.tarefa.tarefas),
+      store,
+      filtro,
+    };
+  },
+  
 });
 </script>
